f7525615b13470e690b9b1a1a97b10c5ec6e123d
ZewdieMc <zewdiehabtie26@gmail.com>
2023-06-01 14:31:12 +0300

    Add test for the rentals in a book class

--- tests/book_spec.rb
+++ tests/book_spec.rb
@@ -1,12 +1,19 @@
 require './classes/book'
+require './classes/person'
 
 describe Book do
   book = Book.new('Rich Dad Poor Dad', 'Robert Kiyosaki')
+  person = Person.new('Lanzz', 21, true)
 
   it 'Should check if the book title was inserted correctly' do
     expect(book.title).to eq('Rich Dad Poor Dad')
   end
   it 'Should check if the book author was inserted correctly' do
     expect(book.author).to eq('Robert Kiyosaki')
+  end
+
+  book.add_rental(person, '2023-1-06')
+  it 'Check if the rental for the book is not empty' do
+    expect(book.rentals).not_to be_empty
   end
 end
